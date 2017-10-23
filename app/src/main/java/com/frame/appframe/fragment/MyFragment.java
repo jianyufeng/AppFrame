222c548c4ec076ca960c234ac1605508922dcf89
jianyufeng <907507643@qq.com>
2017-10-23 16:53:51 +0800

    jian

--- app/src/main/java/com/frame/appframe/fragment/MyFragment.java
+++ app/src/main/java/com/frame/appframe/fragment/MyFragment.java
@@ -9,31 +9,24 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.frame.appframe.R;
-import com.frame.appframe.presenter.DaggerPresenter;
-
-import javax.inject.Inject;
 
 /**
  * Created by Administrator on 2017/10/9.
  */
 
 public class MyFragment extends Fragment {
-    @Inject
-    DaggerPresenter presenter;
+    private TextView tv;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my,container,false);
         tv = view.findViewById(R.id.tv);
-        DaggerFragmentComponent.builder().fragmentModel(new FragmentModel(this)).build().inject(this);
+
         return view;
     }
 
 
 
-    private TextView tv;
-    public void showUserName(String name) {
-        tv.setText(name);
-    }
+
 }
